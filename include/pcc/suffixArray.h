81274109fd8d4f1b3ea5b714c72e93d2b586d3ea
amnv <amnv@cin.ufpe.br>
2017-12-09 23:38:54 -0300

    muita coisa e alguns erros

--- include/pcc/suffixArray.h
+++ include/pcc/suffixArray.h
@@ -8,18 +8,24 @@ class SuffixArray
 {
 private:
     vector<int> suftab;
-    string text;
-    string pattern;
 public:
     vector<int> buildSuffixTab(string text);
     map<string, int> buildSuffixArray(string text);
-    void search(string text, string pattern);
-    void count(map<string, int> suffixArray, string pattern);
+    void count(string text, string pattern);
+    void occ(string text, string pattern);
     int lp(string pattern, string text);
-    int lr(string pattern, string text);
+    int rp(string pattern, string text);
     vector<int> getSuffixTab();
     int lexcmp(string a, string b, int m);
     void debug();
+
+
+    vector<int> search(string text, string pattern, vector<int> Llcp, vector<int> Rlcp);
+    vector<vector<int> > build_P(string text);
+    vector<int> sarr_from_P(vector<int> p);
+    void fillLlcpRlcp(string txt, vector<int> sarr, int l, int r, vector<int> Llcp, vector<int> Rlcp, vector<int> p);
+    int lcp(vector<string> x, vector<string> y);
+    int rsort(vector<pair<int, pair<int, int> > > v, int n);
 };
 
 #endif
